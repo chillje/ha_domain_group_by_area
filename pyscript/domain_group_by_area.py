5696afd1a20234a40b2ce9c82c767995b329be27
chillje <chillje@users.noreply.github.com>
2023-12-19 17:55:11 +0100

    add more error handling

--- pyscript/domain_group_by_area.py
+++ pyscript/domain_group_by_area.py
@@ -3,58 +3,77 @@ from homeassistant.helpers import entity_registry as er
 from homeassistant.helpers import area_registry as ar
 
 def get_all_areas():
-    areareg = ar.async_get(hass)
-
-    all_areas = []
     try:
+        areareg = ar.async_get(hass)
+        all_areas = []
+
         for a in areareg.areas.items():
             id = a[0]
             entry = a[1]
             log.info(f"Area: {entry.id} {entry.name} id: {id}")
             all_areas.append(entry.id)
+
         all_areas = [element.lower() for element in all_areas]
+        return all_areas
+
     except Exception as e:
         log.error(f"Error getting all areas: {e}")
-    return all_areas
+        return []
 
 def get_area_entities(area_name, domain):
+    try:
+        devreg = dr.async_get(hass)
+        entreg = er.async_get(hass)
+        area_entities = []
 
-    devreg = dr.async_get(hass)
-    entreg = er.async_get(hass)
-    area_entities = []
+        for d in devreg.devices.items():
+            device_id = d[0]
+            device_entry = d[1]
 
-    for d in devreg.devices.items():
-        device_id = d[0]
-        device_entry = d[1]
-        if device_entry.area_id and area_name in device_entry.area_id:
-            desired_entity_id = device_entry.id
-            for ent in entreg.entities.items():
-                entity_id = ent[0]
-                entity_entry = ent[1]
-                if entity_entry.device_id and desired_entity_id in entity_entry.device_id:
-                    if entity_entry.entity_id.startswith(f"{domain}."):
-                        if entity_entry.disabled_by is None and entity_entry.hidden_by is None:
-                            area_entities.append(entity_entry.entity_id.strip())
-                        else:
-                            log.info(f"Entry is disabled or hidden: {entity_entry.entity_id}")
-    return area_entities
+            if device_entry.area_id and area_name in device_entry.area_id:
+                desired_entity_id = device_entry.id
+
+                for ent in entreg.entities.items():
+                    entity_id = ent[0]
+                    entity_entry = ent[1]
+
+                    if entity_entry.device_id and desired_entity_id in entity_entry.device_id:
+                        if entity_entry.entity_id.startswith(f"{domain}."):
+                            if entity_entry.disabled_by is None and entity_entry.hidden_by is None:
+                                area_entities.append(entity_entry.entity_id.strip())
+                            else:
+                                log.info(f"Entry is disabled or hidden: {entity_entry.entity_id}")
+
+        return area_entities
+
+    except Exception as e:
+        log.error(f"Error getting area entities: {e}")
+        return []
 
 def filter_blacklist(entities, blacklist):
-    log.debug(f"Filter blacklisted entries: {entities}")
+    try:
+        log.debug(f"Filter blacklisted entries: {entities}")
+        filtered_entities = [entity for entity in entities if entity not in blacklist]
+        log.debug(f"Filtered area entries: {filtered_entities}")
+        return filtered_entities
 
-    filtered_entities = [entity for entity in entities if entity not in blacklist]
-    log.debug(f"Filtered area entries: {filtered_entities}")
-    return filtered_entities
+    except Exception as e:
+        log.error(f"Error filtering blacklist: {e}")
+        return entities
 
 def process_area(area_name, domain, entity_blacklist):
-    group_name = f"{area_name}_all_{domain}s"
-    area_domain_entities = get_area_entities(area_name, domain)
-    filtered_domain_entities = filter_blacklist(area_domain_entities, entity_blacklist)
+    try:
+        group_name = f"{area_name}_all_{domain}s"
+        area_domain_entities = get_area_entities(area_name, domain)
+        filtered_domain_entities = filter_blacklist(area_domain_entities, entity_blacklist)
 
-    if len(filtered_domain_entities) > 0:
-        create_group(group_name, filtered_domain_entities)
-    #else:
-    #    delete_group(group_name)
+        if len(filtered_domain_entities) > 0:
+            create_group(group_name, filtered_domain_entities)
+        #else:
+        #    delete_group(group_name)
+
+    except Exception as e:
+        log.error(f"Error processing area: {e}")
 
 def create_group(group_name, entities):
     try:
@@ -70,7 +89,7 @@ def create_group(group_name, entities):
 def delete_group(group_name):
     try:
         log.info(f"Deleting group: {group_name}")
-        
+
         # Remove the group
         #FIXME not working, state.get throws exception...
         #if state.get(f"group.{group_name}"):
@@ -86,7 +105,6 @@ def create_all(domain, entity_blacklist):
         log.info(f"Create group: {group_name}")
 
         all_entities_of_domain = state.names(domain)
-
         filtered_domain_entities = filter_blacklist(all_entities_of_domain, entity_blacklist)
 
         if len(filtered_domain_entities) > 0:
@@ -94,7 +112,6 @@ def create_all(domain, entity_blacklist):
         
     except Exception as e:
         log.error(f"Error creating \"all_domain\" group of domain: {e}")
-
 
 @service
 def domain_group_by_area(domain, entity_blacklist=None, area_whitelist=None, all=None):
@@ -106,8 +123,7 @@ def domain_group_by_area(domain, entity_blacklist=None, area_whitelist=None, all
             log.info(f"Entity black list: {entity_blacklist}")
 
             if area_whitelist is None:
-                #all_areas = get_all_areas()
-                all_areas = ["buro"]
+                all_areas = get_all_areas()
             else:
                 all_areas = area_whitelist
 
